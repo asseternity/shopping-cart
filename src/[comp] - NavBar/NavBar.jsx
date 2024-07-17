600946642af75ad62cdea343b91c9b71cd1bca60
asseternity <nakupovasset@gmail.com>
2024-07-17 17:15:56 +0500

    feat: create skeleton of the store app with mocks

--- src/[comp] - NavBar/NavBar.jsx
+++ src/[comp] - NavBar/NavBar.jsx
@@ -1,3 +1,34 @@
-const NavBar = () => {};
+import { Link } from 'react-router-dom';
+
+const NavBar = ({ currentTabNameString }) => {
+  const currentStyle = {
+    color: 'red',
+  };
+
+  let homeButtonStyle = {};
+  let shopButtonStyle = {};
+
+  switch (currentTabNameString) {
+    case 'home':
+      homeButtonStyle = currentStyle;
+      break;
+    case 'shop':
+      shopButtonStyle = currentStyle;
+      break;
+    default:
+      break;
+  }
+
+  return (
+    <div>
+      <Link to="/">
+        <button style={homeButtonStyle}>Home</button>
+      </Link>
+      <Link to="/shop">
+        <button style={shopButtonStyle}>Shop</button>
+      </Link>
+    </div>
+  );
+};
 
 export default NavBar;
--- src/[page] - Home/Home.jsx
+++ src/[page] - Home/Home.jsx
@@ -1,3 +1,14 @@
-const Home = () => {};
+import NavBar from '../[comp] - NavBar/NavBar';
+
+const Home = () => {
+  return (
+    <div>
+      <div className="topbar">
+        <NavBar currentTabNameString="home" />
+      </div>
+      <p>I am home</p>
+    </div>
+  );
+};
 
 export default Home;
