bb836867654cb15fbfbe0f9f76efbe25ef95cbff
asseternity <nakupovasset@gmail.com>
2024-07-21 20:36:50 +0500

    feat: styling init. init styled-components

---
+++ src/[comp] - Styled Comps/CartButton.jsx
@@ -0,0 +1,9 @@
+import styled from 'styled-components';
+
+const CartButton = styled.button`
+  border: none;
+  background: none;
+  font-family: 'Lexend';
+`;
+
+export default CartButton;
---
+++ src/[comp] - Styled Comps/ProductCard.jsx
@@ -0,0 +1,10 @@
+import styled from 'styled-components';
+
+const ProductCard = styled.div`
+  height: 650px;
+  width: 300px;
+  font-size: 0.7rem;
+  padding: 0;
+`;
+
+export default ProductCard;
